ea2cad1702bdd923cee4b4e4fae700641ebb7db3
Selçuk Köseoğlu <koseogluselcuk55@gmail.com>
2023-09-21 16:17:36 +0300

    Proje dosyası ekle.

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        // GET: Musteri
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER p1)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var urunler = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(urunler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriGetir(int id)
+        {
+            var mstr = db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mstr);
+        }
+
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERIAD = p1.MUSTERIAD;
+            musteri.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        // GET: Urun
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+        
+        [HttpGet] 
+        public ActionResult YeniUrun()
+        {
+            //DropDownList kullanımı için
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+
+                                             }).ToList();
+            ViewBag.dgr = degerler;   // Viewa taşımak için ViewBag nesnesi oluşturuyoruz.
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniUrun(TBLURUNLER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            p1.TBLKATEGORILER = ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var urunler = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urunler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunGetir(int id)
+        {
+            var urunler = db.TBLURUNLER.Find(id);
+            return View("UrunGetir",urunler);
+        }
+
+        public ActionResult Guncelle(TBLURUNLER p1)
+        {
+            var urun = db.TBLURUNLER.Find(p1.URUNID);
+            urun.FIYAT = p1.FIYAT;
+            urun.URUNAD = p1.URUNAD;
+            urun.URUNKATEGORI = p1.URUNKATEGORI;
+            urun.STOK = p1.STOK;
+            urun.MARKA = p1.MARKA;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
